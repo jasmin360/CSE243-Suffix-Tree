47f0c3a1e5a91d4995e3827629b4fe5ab2805e6b
kim2817 <karma.adnan.hussein@gmail.com>
2025-12-12 14:30:24 +0200

    headerfile suffix

--- Suffix.h
+++ Suffix.h
@@ -1,1 +1,44 @@
-class suffix{}
+//ukonens algorithm 
+
+class suffix{
+public:
+    suffix();
+    ~suffix();
+    class Node{
+    public:
+        Node();
+        ~Node();
+        Node* suffixLink;
+        int index;
+
+    }
+    class End{
+    public:
+        End(int end);
+        ~End();
+        int end;
+    }
+    class Edge{
+    public:
+        Edge(int start, End* end);
+        ~Edge();
+        int start;
+        End* end;
+    }
+    class ActivePoint{
+    public:
+        ActivePoint(Node* node);
+        ~ActivePoint();
+        Node* activeNode;
+        int activeEdge;
+        int activeLength;
+    }
+    Node* root;
+    string text;
+    ActivePoint* activePoint; 
+    int remaining;
+    void insert(int index);
+    void extend(int index);
+    
+
+}
