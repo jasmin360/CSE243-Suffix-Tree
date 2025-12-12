4cabb38c626f7594a181afe0da44c081395772dd
kim2817 <karma.adnan.hussein@gmail.com>
2025-12-12 17:21:13 +0200

    Suffix tree implementation 3shan eyad with errors

--- Suffix.cpp
+++ Suffix.cpp
@@ -14,9 +14,13 @@ class Suffix{
             suffixLink = nullptr;
             index = -1;
         };
+        Node[] child = new Node[6];
         ~Node();
         Node* suffixLink;
         int index;
+        End* end;
+        int start;
+
 
     }
     class End{
@@ -27,16 +31,7 @@ class Suffix{
         ~End();
         int end;
     }
-    class Edge{
-    public:
-        Edge(int start, End* end){
-            this->start = start;
-            this->end = end;
-        };
-        ~Edge();
-        int start;
-        End* end;
-    }
+
     class ActivePoint{
     public:
         ActivePoint(Node* node){
@@ -54,16 +49,43 @@ class Suffix{
     ActivePoint* activePoint; 
     int remaining;
     void insert(int index){
+    };
+
+
+
+    void startPhase(int i){
         remaining++;
         end->end++;
         while(remaining > 0){
             if(activePoint->activeLength == 0){
-                if()
+                if(getNode(i)!=nullptr){
+                    activePoint->activeEdge = i;
+                    activePoint->activeLength++;
+                    break;
+                }
+                else{
+                    Node* leaf = new Node();
+                    leaf->start = i;
+                    leaf->end = end;
+                    activePoint->activeNode->child[text[i]-'A'] = leaf;
+                    remaining--;
+                }
                 activePoint->activeEdge = index;
             }
         }
-
     };
+    Node* getNode (int index){
+        case text[index]{
+            'A': return child[0];
+            'C': return child[1];
+            'G': return child[2];
+            'T': return child[3];
+            '$': return child[4];
+            '#': return child[5];
+        }
+    }
+    
+
     void extend(int index){
 
     };
