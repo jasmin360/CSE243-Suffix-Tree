b3799386db1e1079753e84711c9b7d01d45b4ca4
Eyad-dev <eyadahmedhabib2007@gmail.com>
2025-12-26 19:25:40 +0200

    Functions

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "functions.h"
+#include "Suffix.cpp"
+using namespace std;
+
+namespace DNA{
+    bool SearchPattern(const string& text, const string& pattern){
+        //Wait, Eyad is researching suffix.cpp now
+    }
+
+    //2nd function
+    //3rd function
+    //... other functions
+}
---
+++ functions.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+namespace DNA{
+    bool SearchPattern(const string& text, const string& pattern);
+    //2nd function
+    //3rd function
+    //... other functions
+}
