71a080c6bafbf406f69b7ad4248526dcfae3f93b
Jasmin Ibrahim <jasminibrahimfathy123@gmail.com>
2025-12-29 10:13:32 +0200

    i forgot function.h existed

--- functions.h
+++ functions.h
@@ -10,7 +10,6 @@ namespace DNA {
     );
     std::string* findUniqueRegion(const std::string& sequence,int x);
     void findMaxRepetition(const std::string& sequence, int x, int& count, string& res);
-
     std::string findLargestCommonRegion(const std::string& s1,const std::string& s2);
 
 
