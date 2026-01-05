3eb2b9b5b64ecd166b4e2246109c62891e8f60a9
Jasmin Ibrahim <jasminibrahimfathy123@gmail.com>
2026-01-05 23:49:09 +0200

    THE BACKEND IS CONNECTED I CANNOT BELIEVE MYSELF

--- functions.h
+++ functions.h
@@ -1,18 +1,13 @@
-#pragma once
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
 #include <string>
 
 namespace DNA {
-
-    int* SearchPattern(
-        const std::string& sequence,
-        const std::string& pattern,
-        int& count
-    );
-    std::string* findUniqueRegion(const std::string& sequence,int x);
-    void findMaxRepetition(const std::string& sequence, int x, int& count, const std::string& res);
-    std::string findLargestCommonRegion(const std::string& s1,const std::string& s2);
-
-    std::string findMaxRepetition(const std::string& sequence, int x, int& count, std::string& res);
-
-
+    int* SearchPattern(const std::string&, const std::string&, int&);
+    std::string* findUniqueRegion(const std::string&, int);
+    std::string findMaxRepetition(const std::string&, int, int&, std::string&);
+    std::string findLargestCommonRegion(const std::string&, const std::string&);
 }
+
+#endif
